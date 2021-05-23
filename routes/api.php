a5a34a92a997414199a2095d90a13aea6075bd42
Thiago Mota <thiago@chicorei.com.br>
2021-05-23 16:12:53 -0300

    Create group middleware with sanctum and add auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BedReportsController;
 use App\Http\Controllers\ReportVerificationsController;
 use App\Http\Controllers\VaccineReportsController;
@@ -17,30 +18,38 @@ use App\Http\Controllers\CaseReportsController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/case', [CaseReportsController::class, 'index']);
-Route::get('/case/{caseReport}', [CaseReportsController::class, 'show']);
-Route::post('/case', [CaseReportsController::class, 'store']);
-Route::put('/case/{caseReport}', [CaseReportsController::class, 'update']);
-Route::delete('/case/{caseReport}', [CaseReportsController::class, 'destroy']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
 
-Route::get('/vaccine', [VaccineReportsController::class, 'index']);
-Route::get('/vaccine/{vaccineReport}', [VaccineReportsController::class, 'show']);
-Route::post('/vaccine', [VaccineReportsController::class, 'store']);
-Route::put('/vaccine/{vaccineReport}', [VaccineReportsController::class, 'update']);
-Route::delete('/vaccine/{vaccineReport}', [VaccineReportsController::class, 'destroy']);
+    Route::get('/case', [CaseReportsController::class, 'index']);
+    Route::get('/case/{caseReport}', [CaseReportsController::class, 'show']);
+    Route::post('/case', [CaseReportsController::class, 'store']);
+    Route::put('/case/{caseReport}', [CaseReportsController::class, 'update']);
+    Route::delete('/case/{caseReport}', [CaseReportsController::class, 'destroy']);
 
-Route::get('/bed', [BedReportsController::class, 'index']);
-Route::get('/bed/{bedReport}', [BedReportsController::class, 'show']);
-Route::post('/bed', [BedReportsController::class, 'store']);
-Route::put('/bed/{bedReport}', [BedReportsController::class, 'update']);
-Route::delete('/bed/{bedReport}', [BedReportsController::class, 'destroy']);
+    Route::get('/vaccine', [VaccineReportsController::class, 'index']);
+    Route::get('/vaccine/{vaccineReport}', [VaccineReportsController::class, 'show']);
+    Route::post('/vaccine', [VaccineReportsController::class, 'store']);
+    Route::put('/vaccine/{vaccineReport}', [VaccineReportsController::class, 'update']);
+    Route::delete('/vaccine/{vaccineReport}', [VaccineReportsController::class, 'destroy']);
 
-Route::get('/report-verification', [ReportVerificationsController::class, 'index']);
-Route::get('/report-verification/{reportVerification}', [ReportVerificationsController::class, 'show']);
-Route::post('/report-verification', [ReportVerificationsController::class, 'store']);
-Route::put('/report-verification/{reportVerification}', [ReportVerificationsController::class, 'update']);
-Route::delete('/report-verification/{verificationReport}', [ReportVerificationsController::class, 'destroy']);
+    Route::get('/bed', [BedReportsController::class, 'index']);
+    Route::get('/bed/{bedReport}', [BedReportsController::class, 'show']);
+    Route::post('/bed', [BedReportsController::class, 'store']);
+    Route::put('/bed/{bedReport}', [BedReportsController::class, 'update']);
+    Route::delete('/bed/{bedReport}', [BedReportsController::class, 'destroy']);
+
+    Route::get('/report-verification', [ReportVerificationsController::class, 'index']);
+    Route::get('/report-verification/{reportVerification}', [ReportVerificationsController::class, 'show']);
+    Route::post('/report-verification', [ReportVerificationsController::class, 'store']);
+    Route::put('/report-verification/{reportVerification}', [ReportVerificationsController::class, 'update']);
+    Route::delete('/report-verification/{verificationReport}', [ReportVerificationsController::class, 'destroy']);
+});
+
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
